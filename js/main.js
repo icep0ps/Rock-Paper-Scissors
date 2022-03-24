581b8260d4ae342fe09f08c500486e102b0d973e
icep0ps <tapsmuko@gmail.com>
2022-03-24 15:35:03 +0200

    Added UI and score limiter

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-var playerWins=0
-var playerLoss=0
-var playerTies=0
+var playerWins = 0
+var playerLoss = 0
+var playerTies = 0
+var userwin=false
 const body = document.querySelector('body')
 const info = document.querySelector('.info')
 info.classList.add('game-info')
@@ -9,108 +10,115 @@ const p = document.querySelector("p")
 const img = document.querySelectorAll('.image')
 const imgpc = document.querySelectorAll('.imagepc')
 const button = document.querySelectorAll("button")
+// this gets an image according to the dataset (display none shows nothing while block shows the image)
+img.forEach(function (image) {
+    if (image.dataset.moves !== "none") {
+        image.style.display = "none"
 
-img.forEach(function(image){
-    if(image.dataset.moves!=="none")
-    {
-    image.style.display="none"
-        
-     }
-     else{image.style.display="block" }
+    }
+    else { image.style.display = "block" }
+})
+imgpc.forEach(function (image) {
+    if (image.dataset.moves !== "none") {
+        image.style.display = "none"
+
+    }
+    else { image.style.display = "block" }
+})
+
+
+
+button.forEach(function (buttons) {
+    buttons.addEventListener("click", function () {
+        playerChoice = buttons.dataset.button
+        if (playerLoss <5 && playerWins < 5) {
+        const audio = document.querySelector('audio')
+        audio.currentTime = 0;
+        audio.play()
+            playRound()
+        }
+        else if(playerLoss < 5 ){
+        info.textContent = "You Win"
+        }
+        else{info.textContent = "You Lose"}
+
     })
-imgpc.forEach(function(image){
-        if(image.dataset.moves!=="none")
-        {
-        image.style.display="none"
-            
-         }
-         else{image.style.display="block" }
+})
+
+function playRound(playerSelection, computerPlay) {
+    let letter = ["r", "p", "s"];
+    playerSelection = playerChoice
+    computerPlay = letter[Math.floor(Math.random() * letter.length)]
+
+    function imgSelcPlayer() {
+        img.forEach(function (image) {
+            if (image.dataset.moves !== playerSelection) {
+                image.style.display = "none"
+
+            }
+            else { image.style.display = "block" }
         })
-    
+    }
 
+    function imgSelcComputer() {
+        imgpc.forEach(function (image) {
+            if (image.dataset.moves !== computerPlay) { image.style.display = "none" }
+            else { image.style.display = "block" }
+        })
+    }
 
-button.forEach(function(buttons){
-buttons.addEventListener("click",function (){  
-    playerChoice = buttons.dataset.button
-     playRound()}
-)
-}
-)
-
-function playRound(playerSelection, computerPlay){
-let letter = ["r","p","s"];
-playerSelection = playerChoice
-computerPlay = letter[Math.floor(Math.random() * letter.length)]
-
-function imgSelcPlayer(){
-    img.forEach(function(image){
-        if(image.dataset.moves!==playerSelection)
-        {
-        image.style.display="none"
-            
-         }
-         else{image.style.display="block" }
-        })}
-    
-function imgSelcComputer(){
-imgpc.forEach(function(image){
-if(image.dataset.moves!==computerPlay)
-{image.style.display="none"}
-else{image.style.display="block" }
-                })}
-        
-imgSelcComputer() 
-imgSelcPlayer()
-p.innerText='the computer chose '+ computerPlay
-    if(playerSelection == "r" && computerPlay == "p"){
-        playerLoss ++;
-        info.innerText= "you lose paper beats rock!"
+    imgSelcComputer()
+    imgSelcPlayer()
+    if (playerSelection == "r" && computerPlay == "p") {
+        playerLoss++;
+        info.textContent = "You lose paper beats rock!"
         gameStatus;
     }
 
-    else if (playerSelection == "p" && computerPlay == "r"){
-        playerWins ++;
-        info.innerText="you win paper beats rock!"
-        gameStatus
-    }
-
-    else if (playerSelection == "s" && computerPlay == "r"){
-        playerLoss++ ;   
-        info.innerText= "you lose rock beats scissors!"
-        gameStatus
-    }
-    
-    else if (playerSelection == "s" && computerPlay == "r"){
+    else if (playerSelection == "p" && computerPlay == "r") {
         playerWins++;
-        info.innerText= "you win rock beats scissors!"
+        info.textContent = "You win paper beats rock!"
         gameStatus
     }
 
-    else if (playerSelection == "p" && computerPlay == "s"){
+    else if (playerSelection == "s" && computerPlay == "r") {
         playerLoss++;
-        info.innerText= "you lose scissors beats rock!"
+        info.textContent = "You lose rock beats scissors!"
         gameStatus
     }
 
-    else if (playerSelection == "r" && computerPlay == "s"){
-        playerWins ++ ;
-        info.innerText= "you win scissors beats rock!"
-        gameStatus
-    }
-    
-    
-    else if (playerSelection == "s" && computerPlay == "p"){
-        playerWins ++;
-        info.innerText= "you win scissors beats rock!"
+    else if (playerSelection == "s" && computerPlay == "r") {
+        playerWins++;
+        info.textContent = "You win rock beats scissors!"
         gameStatus
     }
 
-    else{info.innerText= "its a tie"
-        playerTies++; 
-        gameStatus}
-// gameStatus.innerText ='your wins '+ playerWins + '\n computers wins ' + playerLoss
-gameStatus.innerText = playerWins + '-' + playerLoss
+    else if (playerSelection == "p" && computerPlay == "s") {
+        playerLoss++;
+        info.textContent = "You lose scissors beats rock!"
+        gameStatus
+    }
 
-    
+    else if (playerSelection == "r" && computerPlay == "s") {
+        playerWins++;
+        info.textContent = "You win scissors beats rock!"
+        gameStatus
+    }
+
+
+    else if (playerSelection == "s" && computerPlay == "p") {
+        playerWins++;
+        info.textContent = "You win scissors beats rock!"
+        gameStatus
+    }
+
+    else {
+        info.textContent = "Its a tie "
+        playerTies++;
+        gameStatus
+    }
+    gameStatus.textContent = playerWins + '-' + playerLoss
+
+
 }
 
